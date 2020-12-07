63c4c75a5a0546d3f6b2a7109b97726fe0f8df8b
Ramkumar <smb.ram@gmail.com>
2020-12-07 10:39:15 +0530

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -1,19 +1,19 @@
 let icon =
-  '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"></circle>'
+    '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"></circle>'
 
 miro.onReady(() => {
-  miro.initialize({
-    extensionPoints: {
-        getWidgetMenuItems: async (widgets, editmode) => {
-     return [{
-        title: 'Looking Glass',
-        svgIcon: icon,
-        positionPriority: 1,
-        onClick: () => {
-          miro.board.ui.openLeftSidebar('sidebar.html')
+    miro.initialize({
+        extensionPoints: {
+            getWidgetMenuItems: async (widgets, editmode) => {
+                return [{
+                    tooltip: 'Looking Glass',
+                    svgIcon: icon,
+                    positionPriority: 1,
+                    onClick: () => {
+                        miro.board.ui.openLeftSidebar('sidebar.html')
+                    }
+                }]
+            }
         }
-        }]
-      }
-    }
-  })
+    })
 })
